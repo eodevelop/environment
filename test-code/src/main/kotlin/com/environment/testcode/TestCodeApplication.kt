bacca7ffd23a256e48ca02b221fbfb4e66f051c6
eodevelop <eodevelop@naver.com>
2024-02-12 22:33:07 +0900

    first commit

---
+++ test-code/src/main/kotlin/com/environment/testcode/TestCodeApplication.kt
@@ -0,0 +1,11 @@
+package com.environment.testcode
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestCodeApplication
+
+fun main(args: Array<String>) {
+	runApplication<TestCodeApplication>(*args)
+}
---
+++ test-code/src/test/kotlin/com/environment/testcode/TestCodeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.environment.testcode
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestCodeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
