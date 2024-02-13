952387f22778e4e36423a3908e337518ca98551c
eodevelop <eodevelop@naver.com>
2024-02-13 23:29:30 +0900

    1. entity 생성

--- test-code/build.gradle.kts
+++ test-code/build.gradle.kts
@@ -5,6 +5,11 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.4"
 	kotlin("jvm") version "1.9.22"
 	kotlin("plugin.spring") version "1.9.22"
+	kotlin("plugin.noarg") version "1.9.22"
+}
+
+noArg {
+	annotation("javax.persistence.Entity")
 }
 
 group = "com.environment"
@@ -13,6 +18,7 @@ version = "0.0.1-SNAPSHOT"
 java {
 	sourceCompatibility = JavaVersion.VERSION_17
 }
+
 
 repositories {
 	mavenCentral()
